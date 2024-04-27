c114c5756c0255e7f0cdb71f05dbd5a2bff81a96
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2024-04-28 04:09:46 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -18,3 +18,18 @@ terraform {
 
   required_version = "~> 1.0"
 }
+
+
+# terraform/eks.tf
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
+
