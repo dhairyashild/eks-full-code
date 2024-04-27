1396ced826d079071386ff76d9effcf6f42d89dd
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2024-04-28 04:18:31 +0530

    2-vpc.tf

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -31,4 +31,23 @@ module "vpc" {
     "kubernetes.io/role/internal-elb" = "1"
   }
 
+
+terraform/vpc.tf  
+
+node_security_group_additional_rules = {
+    ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+    }
+  }
+
+
 }
+
+
+
+
