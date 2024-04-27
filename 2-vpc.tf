808b7052ec543b7f900bbc783d1555b8c7d4f8df
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2024-04-28 05:06:47 +0530

    2-vpc.tf

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -1,8 +1,6 @@
-# terraform/vpc.tf
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.3"
+  version = "5.0.0"
 
   name = "main"
   cidr = "10.0.0.0/16"
@@ -10,6 +8,13 @@ module "vpc" {
   azs             = ["ap-south-1a", "ap-south-1b"]
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
   public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 
   enable_nat_gateway     = true
   single_nat_gateway     = true
@@ -19,35 +24,6 @@ module "vpc" {
   enable_dns_support   = true
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
-
-
-#terraform/vpc.tf
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
-  }
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-
-
-terraform/vpc.tf  
-
-node_security_group_additional_rules = {
-    ingress_allow_access_from_control_plane = {
-      type                          = "ingress"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      source_cluster_security_group = true
-      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-    }
-  }
-
-
 }
-
-
-
-
